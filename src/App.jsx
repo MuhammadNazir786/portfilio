37bb58791926a10abc309bf8d24ad869d71f80b9
muhammad Nazir <162798214+MuhammadNazir786@users.noreply.github.com>
2026-05-03 13:50:25 +0430

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ import Footer from "./portfilio/Footer";
 
 const App = () => {
   const data = {
-    name: "Nazir Zarifi",
-    title: "Full stack Developer & UI/UX Designer",
+    name: "Mohammad Nazir Zarifi",
+    title: "MERN & Next.js Developer || UI/UX designer",
     description:
       "Full-Stack Developer passionate about building scalable, responsive, and user-friendly web applications. Skilled in Node.js, React, Next.js, HTML, and CSS. Experienced in creating clean, maintainable frontend and backend solutions, delivering exceptional user experiences through best practices in full-stack development.",
     imageUrl: profil, // Replace with your image URL
